8a25c6276f8a3e13655ef631f6a2985913b1ab91
protocol-designer <77050383+protocol-designer@users.noreply.github.com>
2022-01-22 13:19:45 +0530

    Updated some parameters

--- src/token-supply.js
+++ src/token-supply.js
@@ -17,7 +17,7 @@ const metrics = [
         preMint: 100000,
         bonus: 0,
         vestingStart: getTime(TGE, 30),
-        vestingEnds: getTime(getTime(TGE, 30), 9*30),
+        vestingEnds: getTime(getTime(TGE, 30), 30*9),
     },
     {
         name: "Private Round 2",
@@ -25,7 +25,7 @@ const metrics = [
         preMint: 204364,
         bonus: 0,
         vestingStart: getTime(TGE, 30),
-        vestingEnds: getTime(getTime(TGE, 30), 9*30),
+        vestingEnds: getTime(getTime(TGE, 30), 30*9),
     },
     {
         name: "Reward Pool",
@@ -41,7 +41,7 @@ const metrics = [
         preMint: 0,
         bonus: 10,
         vestingStart: getTime(TGE, 30*9),
-        vestingEnds: getTime(getTime(TGE, 30*9), 48*30),
+        vestingEnds: getTime(getTime(TGE, 30*9), 30*48),
     },
     {
         name: "Foundation",
@@ -49,15 +49,15 @@ const metrics = [
         preMint: 0,
         bonus: 0,
         vestingStart: getTime(TGE, 30*18),
-        vestingEnds: getTime(getTime(TGE, 30*9), 39*30),
+        vestingEnds: getTime(getTime(TGE, 30*18), 39*30),
     },
     {
         name: "Advisor",
         totalTokens: 2000000,
         preMint: 200000,
-        bonus: 10,
-        vestingStart: getTime(TGE, 30*3),
-        vestingEnds: getTime(getTime(TGE, 30*3), 30*48),
+        bonus: 20,
+        vestingStart: getTime(TGE, 30*6),
+        vestingEnds: getTime(getTime(TGE, 30*6), 30*48),
     },
     {
         name: "Ecosystem",
